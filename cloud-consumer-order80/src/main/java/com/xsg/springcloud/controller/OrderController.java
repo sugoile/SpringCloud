ba5ced7943feffb0ff694035c3726a4072e30ca2
xsg <160487079@qq.com>
2020-12-02 17:22:10 +0800

    Sleuth+zipkin

--- cloud-consumer-order80/src/main/java/com/xsg/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/xsg/springcloud/controller/OrderController.java
@@ -48,20 +48,26 @@ public class OrderController {
 
 
     @GetMapping(value = "/consumer/payment/serverport")
-    public String getPaymentLB()
-    {
+    public String getPaymentLB() {
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
 
-        if(instances == null || instances.size() <= 0)
-        {
+        if (instances == null || instances.size() <= 0) {
             return null;
         }
 
         ServiceInstance serviceInstance = myLoadBalanced.instances(instances);
         URI uri = serviceInstance.getUri();
 
-        return restTemplate.getForObject(uri+"/payment/serverport",String.class);
+        return restTemplate.getForObject(uri + "/payment/serverport", String.class);
 
+    }
+
+    // ====================> zipkin+sleuth
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin()
+    {
+        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
+        return result;
     }
 
 }
--- cloud-provider-payment8001/src/main/java/com/xsg/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/xsg/springcloud/controller/PaymentController.java
@@ -32,28 +32,24 @@ public class PaymentController {
     private String Server;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(@RequestBody Payment payment)
-    {
+    public CommonResult create(@RequestBody Payment payment) {
         Long result = paymentService.create(payment);
-        log.info("*****插入结果："+result);
-        if(result > 0)
-        {
-            return new CommonResult(200,"插入数据库成功, Server: " + Server, result);
-        }else{
-            return new CommonResult(444,"插入数据库失败",null);
+        log.info("*****插入结果：" + result);
+        if (result > 0) {
+            return new CommonResult(200, "插入数据库成功, Server: " + Server, result);
+        } else {
+            return new CommonResult(444, "插入数据库失败", null);
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id)
-    {
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
 
-        if(payment != null)
-        {
-            return new CommonResult(200,"查询成功, Server: " + Server, payment);
-        }else{
-            return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
+        if (payment != null) {
+            return new CommonResult(200, "查询成功, Server: " + Server, payment);
+        } else {
+            return new CommonResult(444, "没有对应记录,查询ID: " + id, null);
         }
     }
 
@@ -64,28 +60,35 @@ public class PaymentController {
      * instances 通过传入某一service的id来返回 接口、主机等信息
      */
     @GetMapping(value = "/payment/discoveryClient")
-    public Object discoveryClient(){
+    public Object discoveryClient() {
         List<String> services = discoveryClient.getServices();
-        for (String service : services){
-                log.info("\"" + service + "\" ");
+        for (String service : services) {
+            log.info("\"" + service + "\" ");
         }
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-        for (ServiceInstance instance : instances){
+        for (ServiceInstance instance : instances) {
             log.info(instance.getServiceId() + "\t" + instance.getPort() + "\t" + instance.getUri());
         }
         return this.discoveryClient;
     }
 
     @GetMapping(value = "/payment/serverport")
-    public String serverport(){
+    public String serverport() {
         return Server;
     }
 
     @GetMapping(value = "/payment/feign/timeout")
-    public String paymentFeignTimeout()
-    {
+    public String paymentFeignTimeout() {
         // 业务逻辑处理正确，但是需要耗费3秒钟
-        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return Server;
     }
+
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin() { return "zipkin-Sleuth";}
+
 }
